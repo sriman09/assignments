f8405c853df21a4be55c8edbc1727e42d8b003f4
sriman09 <39696623+sriman09@users.noreply.github.com>
2023-12-05 00:52:17 +0530

    todo list solved

--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -11,7 +11,34 @@
 */
 
 class Todo {
+  constructor() {
+    this.todos = [];
+  }
+  add(todo) {
+    this.todos.push(todo);
+  }
 
+  remove(indexOfTodo) {
+    this.todos = this.todos.filter(
+      (item) => this.todos.indexOf(item) !== indexOfTodo
+    );
+  }
+
+  update(index, updatedTodo) {
+    if (index < this.todos.length) this.todos[index] = updatedTodo;
+  }
+
+  getAll() {
+    return this.todos;
+  }
+
+  get(indexOfTodo) {
+    if (indexOfTodo < this.todos.length) return this.todos[indexOfTodo];
+    else return null;
+  }
+  clear() {
+    this.todos = [];
+  }
 }
 
 module.exports = Todo;
